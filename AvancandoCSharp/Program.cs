1caee1cb873357c3081fbf6d18e32fcd7a4f93ce
Hercules <herculeslcdev@gmail.com>
2020-03-03 22:54:36 -0300

    Trabalhando com Strings - Funções de string

---
+++ AvancandoCSharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+
+namespace AvancandoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Hoje à noite, sem luz, decidi xeretar a quinta gaveta de vovô: achei lingüiça, pão e fubá";
+
+            // realiza replace independente de CAPS LOCK
+            string frase_trocada = frase.Replace("VOVô", "mamãe", true, CultureInfo.CurrentCulture);
+            WriteLine(frase_trocada);
+
+            /*
+            WriteLine(frase.ToUpper());
+            WriteLine(frase.ToLower());
+            */
+
+            /* pegar fatias da string
+            string subFrase = "achei";
+            var posInicial = frase.IndexOf(subFrase);
+            var posFinal = 10;
+            string sub = frase.Substring(posInicial);
+            WriteLine(sub);
+            */
+            /*
+            var pos = frase.IndexOf(subFrase);
+            WriteLine(pos);
+            */
+            /* se a frase contem determinada string
+            var achou = frase.Contains(subFrase);
+            WriteLine(achou);
+            */
+            //WriteLine(frase.Length); // contar caracteres
+
+            /* dividir string 
+            string[] partes = frase.Split(",");
+            for(var i=0; i < partes.Length;i++)
+            {
+                WriteLine(partes[i].Trim());
+            }
+            */
+            ReadLine();
+        }
+    }
+}
